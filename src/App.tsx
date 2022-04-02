53171998af1011d21eeb55b6f4e028240792c877
UserPasha <pasharoma13@gmail.com>
2022-04-02 17:49:36 +0300

    change state to array of objects and add difficult, rating

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,9 @@ function App() {
         title: string
         answers: Array<string>
     }
+    type decidedTasksArratType=Array<decidedTasksType>
 
-    const decidedTasks =
+    const decidedTasks =[
         {
             id: v1(),
             title: 'Deodorant Evaporator',
@@ -27,18 +28,47 @@ function App() {
                 ,'function evaporator(content, evap_per_day, threshold){threshold = threshold / 100;evap_per_day = evap_per_day / 100;' +
                 'return Math.ceil(Math.log(threshold) / Math.log(1-evap_per_day))}'
                 ,'with (Math) evaporator = (唐,ev,t) => ceil( log(t/100) / log(1-ev/100) )'
-            ]
+            ],
+            difficult: '8 kyu      ',
+            rating: '    3 star'
 
-        }
+        },
+        {
+            id: v1(),
+            title: 'Test',
+            description: 'test test tes tes test This program tests the life of an evaporator containing a gas.\n' +
+                '\n' +
+                'We know the content of the evaporator (content in ml), the percentage of foam or gas lost every day (evap_per_day) and the threshold (threshold) in percentage beyond which the evaporator is no longer useful. All numbers are strictly positive.\n' +
 
-  return (
+                'Example:\n' +
+                'evaporator(10, 10, 5) -> 29',
+            answers:[' evap_per_dafunction evaporator(content, evap_per_day,  evap_per_dathreshold){ let i= 0;let x=threshold*content/100 while( x<content){' +
+            'content=content-content*evap_per_day/100; i++}return i}'
+                ,'function evapora evap_per_dator(content, evap_per_day, threshold){threshold = threshold / 100;evap_per_day = evap_per_day / 100;' +
+                'return Math  evap_per_daevap_per_da.ceil(Math.log(threshold) / Math.log(1-evap_per_day))}'
+                ,'with (Math)  evap_per_daevaporator = (唐,ev,t) => ceil( log(t/ evap_per_da100) / log(1-ev/100) )'
+            ],
+            difficult: '7 kyu      ',
+            rating: '    4 star'
+
+        }]
+    const mappedWrappers = decidedTasks.map(m=>  <InfoBoard title={m.title}
+                                                             description={m.description}
+                                                             answers={m.answers}
+                                                             key={m.id}
+                                                            difficult={m.difficult}
+                                                            rating={m.rating}
+     />)
+
+
+    return (
     <div className="wrapper">
     {/*Hello! Im a your helper! can i help?*/}
-      <InfoBoard title={decidedTasks.title}
-                 description={decidedTasks.description}
-                 answers={decidedTasks.answers}
-      />
-
+    {/*  <InfoBoard title={decidedTasks.title}*/}
+    {/*             description={decidedTasks.description}*/}
+    {/*             answers={decidedTasks.answers}*/}
+    {/*  />*/}
+        {mappedWrappers}
     </div>
   );
 }
--- src/components/InfoBoard.tsx
+++ src/components/InfoBoard.tsx
@@ -2,22 +2,29 @@ import React from 'react';
 import c from "./InfoBoard.module.css"
 import {DoorLock} from "./DoorLock";
 
-type InfoBoardType ={
+type InfoBoardType = {
     title: string
     description: string
     answers: Array<string>
+    difficult: string
+    rating: string
 }
 
 export const InfoBoard = (props: InfoBoardType) => {
 
     return (
-        <div className={c.wrapper}>how
-            <div className={c.title}><h2>{props.title}</h2></div>
-            <div className={c.body}><div className={c.text}>
-                {props.description}
+        <div className={c.wrapper}>
+            <div className={c.tags}>
+               <div>{props.difficult}</div>
+               <div>{props.rating}</div>
             </div>
+            <div className={c.title}><h2>{props.title}</h2></div>
+            <div className={c.body}>
+                <div className={c.text}>
+                    {props.description}
+                </div>
                 <div className={c.doorLock}>
-                  <DoorLock answers={props.answers}/>
+                    <DoorLock answers={props.answers}/>
                 </div>
             </div>
 
